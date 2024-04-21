a03e537923b366808b2e725bfbcfa3f0d409731f
ichichavez <isidrochavez429@gmail.com>
2024-04-21 00:15:37 -0400

    Initial Commit

---
+++ AirbnbClone/AirbnbCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirbnbCloneApp.swift
+//  AirbnbClone
+//
+//  Created by Isidro Chávez on 2024-04-21.
+//
+
+import SwiftUI
+
+@main
+struct AirbnbCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
